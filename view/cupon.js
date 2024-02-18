2ef775ad2f353348609e4e057d71419ad426c2b2
Rpyung23 <guaman1579@gmail.com>
2024-02-17 21:13:46 -0500

    update

--- view/cupon.js
+++ view/cupon.js
@@ -105,15 +105,22 @@ app.post("/static",async function(req,res)
         var data1 = await CuponController.staticCuponVendidoController(req.body.email)
         var data2 = await CuponController.staticCuponOcupadoController(req.body.email)
 
+        data1.cant_cupon_vn = data1.cant_cupon_vn != undefined && data1.cant_cupon_vn != null ? parseInt(data1.cant_cupon_vn) : 0
+        data1.disponible_cupon_vn = data1.disponible_cupon_vn != undefined && data1.disponible_cupon_vn != null ? parseInt(data1.disponible_cupon_vn) : 0
+        data1.tot_vn = data1.tot_vn != undefined && data1.tot_vn != null ? parseInt(data1.tot_vn) : 0
+        data1.cant_cupon_ocp = data2.cant_cupon_ocp != undefined && data2.cant_cupon_ocp != null ? parseInt(data2.cant_cupon_ocp) : 0
+        data1.disponible_cupon_ocp = data2.disponible_cupon_ocp != undefined && data2.disponible_cupon_ocp != null ? parseInt(data2.disponible_cupon_ocp) : 0
+        data1.tot_ocp =  data2.tot_ocp != undefined && data2.tot_ocp != null ? parseInt(data2.tot_ocp) : 0
+
         res.status(200).json({
             status_code : 200,
             msm : "OK",
-            cant_cupon_vn: data1.cant_cupon_vn != undefined && data1.cant_cupon_vn != null ? parseInt(data1.cant_cupon_vn) : 0,
-            disponible_cupon_vn: data1.disponible_cupon_vn != undefined && data1.disponible_cupon_vn != null ? parseInt(data1.disponible_cupon_vn) : 0,
-            tot_vn: data1.tot_vn != undefined && data1.tot_vn != null ? parseInt(data1.tot_vn) : 0,
-            cant_cupon_ocp: data2.cant_cupon_ocp != undefined && data2.cant_cupon_ocp != null ? parseInt(data2.cant_cupon_ocp) : 0,
-            disponible_cupon_ocp: data2.disponible_cupon_ocp != undefined && data2.disponible_cupon_ocp != null ? parseInt(data2.disponible_cupon_ocp) : 0,
-            tot_ocp: data2.tot_ocp != undefined && data2.tot_ocp != null ? parseInt(data2.tot_ocp) : 0
+            cant_cupon_vn: (data1.tot_vn/data1.cant_cupon_vn)*100,
+            disponible_cupon_vn: (data1.tot_vn/data1.disponible_cupon_vn)*100,
+            tot_vn: data1.tot_vn,
+            cant_cupon_ocp: (data2.tot_ocp/data2.cant_cupon_ocp)*100,
+            disponible_cupon_ocp: (data2.tot_ocp/data2.disponible_cupon_ocp)*100,
+            tot_ocp: data2.tot_ocp
         })
     }catch (e) {
         res.status(200).json({
